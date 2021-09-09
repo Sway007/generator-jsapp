e1cd6e2bc1980c05e0833abc638d51728cdef185
swayyan <yanshiwei.ysw@antgroup.com>
2021-09-09 14:06:23 +0800

    feat: add less

--- templates/react/config/webpack.common.js
+++ templates/react/config/webpack.common.js
@@ -1,29 +1,38 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 const PROJ_ROOT = path.dirname(__dirname);
 
 module.exports = {
   entry: {
-    main: path.resolve(PROJ_ROOT, "src/index.tsx"),
+    main: path.resolve(PROJ_ROOT, 'src/index.tsx'),
   },
   output: {
-    path: path.resolve(PROJ_ROOT, "dist"),
-    filename: "index.[contenthash].js",
+    path: path.resolve(PROJ_ROOT, 'dist'),
+    filename: 'index.[contenthash].js',
   },
   resolve: {
-    extensions: [".ts", ".tsx", ".js", ".jsx"],
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        loader: "babel-loader",
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.less$/,
+        use: [
+          // compiles Less to CSS
+          'style-loader',
+          'css-loader',
+          'less-loader',
+        ],
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "index.html",
+      template: 'index.html',
     }),
   ],
 };
